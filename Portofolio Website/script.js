70443af6f2e56f4df0ada962d4f33bd124ce06a9
Richardrflsn <105922097+Richardrflsn@users.noreply.github.com>
2023-10-04 22:01:33 +0700

    on progress portofolio section ( bug :) )

--- Portofolio Website/script.js
+++ Portofolio Website/script.js
@@ -14,3 +14,19 @@ const textLoad = () => {
 
 textLoad();
 setInterval(textLoad, 12000);
+
+$(document).on('click', '.portofolio-filter li', function(){
+    $(this).addClass('portofolio-filter-active').siblings().removeClass('portofolio-filter-active')
+});
+
+$(document).ready(function() {
+    $('.list').click(function(){
+        const value = $(this).attr('data-filter');
+        if(value == 'all'){
+            $('.project-box').show('1000');
+        } else {
+            $('.project-box').not('.'+value).hide('1000');
+            $('.project-box').filter('.'+value).show('1000');
+        }
+    })
+})
